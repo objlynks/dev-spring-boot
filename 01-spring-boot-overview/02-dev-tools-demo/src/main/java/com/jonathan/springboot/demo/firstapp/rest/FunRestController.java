50647457eba01774857c785a4b711689805dffd0
objlynks <obedjonathan26@gmail.com>
2023-03-24 20:30:08 +0100

    01-spring-boot demo

---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/jonathan/springboot/demo/firstapp/rest/FunRestController.java
@@ -0,0 +1,11 @@
+package com.jonathan.springboot.demo.firstapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+        public String sayHello(){
+        return "Hello world!";
+    }
+}
